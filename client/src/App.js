34333ef361f94589c23f58c30f93c5320f8fb4d5
nintichance <mintidance@gmail.com>
2024-05-25 18:32:35 -0400

    Add oscillator component

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import './App.scss';
+import Osc1 from './components/Osc1';
 
 let actx = new AudioContext()
 let out = actx.destination
@@ -8,11 +9,18 @@ let gain1 = actx.createGain()
 osc1.connect(gain1)
 gain1.connect(out)
 function App() {
+
+  const changeOsc1Freq = (e) => {
+    console.log(e.target.value)
+  }
+  
   return (
     <div className="App">
       <h1>Sliders</h1>
       <button onClick={()=> osc1.start()}>start</button>
       <button onClick={()=> osc1.stop()}>stop</button>
+      <Osc1 changeFreq={changeOsc1Freq}/>
+
     </div>
   )
 }
