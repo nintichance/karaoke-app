740f93197a1088f0bb226bda93e6af610977851f
VinodS7 <vinodv.ssubramanian@gmail.com>
2024-05-25 20:39:15 -0400

    Add ability to start and stop stream but errors when starting again

--- client/src/App.js
+++ client/src/App.js
@@ -9,29 +9,29 @@ import AudioStreamer from './components/AudioStream';
 function App() {
 
   const audioCtx = new AudioContext()
-  let out = audioCtx.destination
+  // let out = audioCtx.destination
   let osc1 = audioCtx.createOscillator()
-  let gain1 = audioCtx.createGain()
-  
-  osc1.connect(gain1)
-  gain1.connect(out)
-  const [osc1Freq, setOsc1Freq] = useState([])
-  const changeOsc1Freq = (e) => {
-    console.log(e.target.value)
-    let { value } = e.target
-    setOsc1Freq(value)
-    osc1.frequency.value = value
-  }
+  // let gain1 = audioCtx.createGain()
+
+  // osc1.connect(gain1)
+  // gain1.connect(out)
+  // const [osc1Freq, setOsc1Freq] = useState([])
+  // const changeOsc1Freq = (e) => {
+  //   console.log(e.target.value)
+  //   let { value } = e.target
+  //   setOsc1Freq(value)
+  //   osc1.frequency.value = value
+  // }
 
   
   return (
     <div className="App">
       <h1>Sliders</h1>
       <audio></audio>
-      <button onClick={()=> osc1.start()}>start</button>
-      <button onClick={()=> osc1.stop()}>stop</button>
-      <Osc1 changeFreq={changeOsc1Freq} freq={osc1Freq}/>
-      <AudioStreamer audioCtx={audioCtx}/>
+      {/* <button onClick={()=> osc1.start()}>start</button>
+      <button onClick={()=> osc1.stop()}>stop</button> */}
+      {/* <Osc1 changeFreq={changeOsc1Freq} freq={osc1Freq}/> */}
+      <AudioStreamer audioCtx={audioCtx} osc1={osc1}/>
 
     </div>
   )
--- client/src/components/AudioStream.js
+++ client/src/components/AudioStream.js
@@ -1,33 +1,41 @@
-import React, { useRef, useState } from 'react';
-const AudioStreamer = ({audioCtx}) => {
-  const [recordedUrl, setRecordedUrl] = useState('');
-  const mediaStream = useRef(null);
-  const chunks = useRef([]);
+import React, { useRef, useState } from 'react'
+const AudioStreamer = ({audioCtx, osc1}) => {
+  const mediaStream = useRef(null)
   const startStreaming = async () => {
+    osc1.start()
+    osc1.stop()
     try {
       const stream = await navigator.mediaDevices.getUserMedia(
         { audio: true }
-      );
-      mediaStream.current = stream;
+      )
+      mediaStream.current = stream
      
       const source = new MediaStreamAudioSourceNode(audioCtx, {
         mediaStream: stream,
-      });
+      })
 
-      let gain1 = audioCtx.createGain()
+      // let gain1 = audioCtx.createGain()
       let out = audioCtx.destination
-      source.connect(gain1)
-      gain1.connect(out)
+      source.connect(out)
+      // gain1.connect(out)
       console.log("It got here")
     } catch (error) {
-      console.error('Error accessing microphone:', error);
+      console.error('Error accessing microphone:', error)
     }
-  };
+  }
+  const stopStreaming = () => {
+    if (mediaStream.current){
+      mediaStream.current.getTracks().forEach((track)=>{
+        console.log("got it")
+        track.stop()
+      })
+    }
+  }
   return (
     <div>
-      <audio controls src={recordedUrl} />
       <button onClick={startStreaming}>Start Streaming</button>
+      <button onClick={stopStreaming}>Stop Streaming</button>
     </div>
-  );
-};
-export default AudioStreamer;
+  )
+}
+export default AudioStreamer
